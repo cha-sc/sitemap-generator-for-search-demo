97eb649b4238ddca2eac58552c56fa27dc5ed3e9
Chris Haddad <cha@sitecore.net>
2026-09-24 13:15:22 -0400

    fixing something that bothers me, setting the default value to the same as the placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -192,6 +192,7 @@ export default function Home() {
             min="1"
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
             placeholder="100"
+            defaultValue="100"
           />
         </div>
         <button
